c49f8231560e14bafcc5f7fceb05c79ea326790c
Mhh123 <905690216@qq.com>
2017-10-26 01:51:49 +0800

    aa

--- drag.js
+++ drag.js
@@ -9,15 +9,6 @@
   <title>Document</title>
  </head>
  <body><!--网页主题-->
-  <div style="width:100px;height:100px;background:red;">
-	  </div><!--块-->
-	  <div></div><!--双标签-->
-	  <img/><!--单标签-->
-	  <img/><!--单标签-->
-	  <img/><!--单标签-->
-	  <img/><!--单标签-->
-	  <span>1</span>
-	  <span>1</span>
-	  <span>1</span>
+  
  </body>
 </html>
