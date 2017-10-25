e6ab1bd3b78b4eb5446766c141d66a691c320780
Mhh123 <905690216@qq.com>
2017-10-26 01:03:00 +0800

    creat two

---
+++ drag.js
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus?">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <title>Document</title>
+ </head>
+ <body><!--网页主题-->
+  <div style="width:100px;height:100px;background:red;">
+	  </div><!--块-->
+	  <div></div><!--双标签-->
+	  <img/><!--单标签-->
+ </body>
+</html>
