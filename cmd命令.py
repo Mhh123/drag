7039774677110ed63f99758bea306cb6831c64b9
Mhh123 <905690216@qq.com>
2017-10-26 20:35:04 +0800

    change2

---
+++ cmd命令.py
@@ -0,0 +1,13 @@
+1创建和删除
+md 是mkdir的简写
+mkdir
+例如：
+	mkdir test1  //创建一个空目录
+	mkdir  -p test2/test22 //递归创建多个目录
+
+rd 是rmdir的简写
+rmdir
+例如：
+	rmdir /s/q filename
+	/s 除了删除目录本身还将删除目录下的子目录
+	/q 安静模式、带/s删除目录会提示yes or no
